ca4f88a0284afe995e2691b07bb851ee188a8e88
ismahaneyy <liciousmayrah@gmail.com>
2023-12-01 20:30:33 +0300

    send money

---
+++ c++/if.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int test = 20;
+    if (test <= 10){
+        cout << "The output is " <<test << endl;
+    }
+     cout << test << endl;
+
+}
---
+++ c++/send money.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int phonenumber;
+    float amount;
+    cout << "Enter phone no." << endl;
+    cin >>phonenumber;
+    cout << "Enter Amount" << endl;
+    cin >> amount;
+    cout <<"You entered phone number:"<< phonenumber << endl;
+    cout <<"You entered:"<< amount << endl;
+
+    return 0;
+}
