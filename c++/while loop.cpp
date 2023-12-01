2ff1826f3e18aec5dafb1b965f95d6bd7f1b9060
ismahaneyy <liciousmayrah@gmail.com>
2023-12-01 20:28:31 +0300

    while loop

---
+++ c++/while loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int i;
+    // for (int i{10}; i > 0; --i) 
+    //     cout << i << endl;
+    int i{10};
+        while(i  >0){
+            --i;
+              cout <<i << endl;                                                                                                                   VBGGGG                                                                                                                                                                                                                                                                                                                                                                                                                                                                       3                                                                                                                                             S                                     EEEEEDDDDDDDDDDDDDDDDD SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        G
+        }
+       
+    
+    return 0;
+}
